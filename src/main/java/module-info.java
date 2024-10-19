d78e1c8c56d5a57a52ffa0a60865f109ca044cc5
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2024-10-19 14:43:31 +0530

    Add Lombok dependency to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module lk.kavishka.ijse.tictactoe {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
+    requires static lombok;
 
 
     opens lk.kavishka.ijse.tictactoe to javafx.fxml;
