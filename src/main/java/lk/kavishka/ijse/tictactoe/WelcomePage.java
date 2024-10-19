6a994c840fb077796ae64438b54197ec57039791
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2024-10-19 16:30:42 +0530

    Added WelcomePage class and corresponding FXML file for the initial game setup screen.

---
+++ src/main/java/lk/kavishka/ijse/tictactoe/WelcomePage.java
@@ -0,0 +1,37 @@
+package lk.kavishka.ijse.tictactoe;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class WelcomePage {
+
+    @FXML
+    private JFXButton btnEnter;
+
+    @FXML
+    private JFXButton btnO;
+
+    @FXML
+    private JFXButton btnX;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnOOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnXOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterOnAction(ActionEvent event) {
+
+    }
+
+}
